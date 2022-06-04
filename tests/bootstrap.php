62d009ed1c488fd7a9b98aeb68137a06069293f9
lkeme <1462364497@qq.com>
2022-06-04 15:52:09 +0800

    [feat] DebugCommand

---
+++ tests/bootstrap.php
@@ -0,0 +1,41 @@
+<?php declare(strict_types=1);
+
+/**
+ *  Website: https://mudew.com/
+ *  Author: Lkeme
+ *  License: The MIT License
+ *  Email: Useri@live.cn
+ *  Updated: 2022 ~ 2023
+ *
+ *   _____   _   _       _   _   _   _____   _       _____   _____   _____
+ *  |  _  \ | | | |     | | | | | | | ____| | |     |  _  \ | ____| |  _  \ &   ／l、
+ *  | |_| | | | | |     | | | |_| | | |__   | |     | |_| | | |__   | |_| |   （ﾟ､ ｡ ７
+ *  |  _  { | | | |     | | |  _  | |  __|  | |     |  ___/ |  __|  |  _  /  　 \、ﾞ ~ヽ   *
+ *  | |_| | | | | |___  | | | | | | | |___  | |___  | |     | |___  | | \ \   　じしf_, )ノ
+ *  |_____/ |_| |_____| |_| |_| |_| |_____| |_____| |_|     |_____| |_|  \_\
+ */
+
+error_reporting(E_ALL | E_STRICT);
+date_default_timezone_set('Asia/Shanghai');
+
+$libDir = dirname(__DIR__);
+$npMap = [
+    'Tests\\' => $libDir . '/tests/',
+    "Bhp\\" => $libDir . '/src/',
+];
+
+spl_autoload_register(static function ($class) use ($npMap) {
+    foreach ($npMap as $np => $dir) {
+        $file = $dir . str_replace('\\', '/', substr($class, strlen($np))) . '.php';
+
+        if (file_exists($file)) {
+            include $file;
+        }
+    }
+});
+
+if (is_file(dirname(__DIR__, 3) . '/autoload.php')) {
+    require dirname(__DIR__, 3) . '/autoload.php';
+} elseif (is_file(dirname(__DIR__) . '/vendor/autoload.php')) {
+    require dirname(__DIR__) . '/vendor/autoload.php';
+}
