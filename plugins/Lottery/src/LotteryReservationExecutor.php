11d0046cef583be2503d344dc95a624e48a76bff
Lkeme <19500576+lkeme@users.noreply.github.com>
2026-04-11 15:08:29 +0800

    fix: rearm login on lottery auth failures

--- plugins/Lottery/src/LotteryReservationExecutor.php
+++ plugins/Lottery/src/LotteryReservationExecutor.php
@@ -3,15 +3,20 @@
 namespace Bhp\Plugin\Builtin\Lottery;
 
 use Bhp\Api\Support\ApiJson;
+use Bhp\Login\AuthFailureClassifier;
 use Bhp\Request\Request;
+use Bhp\Util\Exceptions\NoLoginException;
 use Throwable;
 
 class LotteryReservationExecutor
 {
+    private AuthFailureClassifier $authFailureClassifier;
+
     public function __construct(
         private readonly LotteryReservationService $reservationService,
         private readonly Request $request,
     ) {
+        $this->authFailureClassifier = new AuthFailureClassifier();
     }
 
     /**
@@ -26,6 +31,10 @@ class LotteryReservationExecutor
         $request = $this->reservationService->buildReserveRequest($lottery, $csrf, $referer);
 
         $response = $this->postJson($request['url'], $request['payload'], $request['headers']);
+        $this->assertNotAuthFailure($response, sprintf(
+            '抽奖: 预约 ReserveId=%s 时账号未登录',
+            (string)($lottery['rid'] ?? ''),
+        ));
 
         return $this->reservationService->evaluateReserveResponse($response, $lottery);
     }
@@ -48,5 +57,14 @@ class LotteryReservationExecutor
         }
 
         return ApiJson::decode($raw, 'lottery.reserve');
+    }
+
+    /**
+     * @param array<string, mixed> $response
+     * @throws NoLoginException
+     */
+    protected function assertNotAuthFailure(array $response, string $fallbackMessage): void
+    {
+        $this->authFailureClassifier->assertNotAuthFailure($response, $fallbackMessage);
     }
 }
